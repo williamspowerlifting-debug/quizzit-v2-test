98c669a1ceba95bc7479050d601de60dc42ad4fb
williamspowerlifting-debug <williamspowerlifting@gmail.com>
2026-09-13 02:49:07 +0300

    Delete frontend directory

--- frontend/src/config.ts
+++
@@ -1,8 +0,0 @@
-export const config = {
-  workerUrl:
-    import.meta.env.VITE_WORKER_URL ||
-    "https://gapfill-ai-googledrive-dev.williamspowerlifting.workers.dev",
-  googleClientId:
-    "187762853414-v7e6kqi1e86pkf0a2s1b92c6q245ralc.apps.googleusercontent.com",
-  bunnyTusEndpoint: "https://video.bunnycdn.com/tusupload",
-};
