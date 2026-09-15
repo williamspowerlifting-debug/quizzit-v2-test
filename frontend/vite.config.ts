ee1dc022b8ba479f3fccf86461efccb49ac438fb
williamspowerlifting-debug <williamspowerlifting@gmail.com>
2026-09-15 15:49:47 +0300

    Delete frontend directory

--- frontend/vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-export default defineConfig({
-  plugins: [react()],
-  // This repository is intended to be deployed as:
-  // https://<YOUR-GITHUB-USERNAME>.github.io/quizzit-v2-test/
-  base: "/quizzit-v2-test/",
-});
